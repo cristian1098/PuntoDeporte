fce036bb2c829865be561823baee23266d25100b
cristian1098 <cfernandezpando@gmail.com>
2022-05-02 12:08:46 -0300

    punto deporte Fernandez

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import './NavBar.css'
+import React from "react";
+import "./NavBar.css";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      className="logo fixed-top navbar-height"
+    >
+      <Container>
+        <h4 id="nav-bar-logo">Punto deporte</h4>
 
-export default NavBar
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Categorias" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Calzado</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Indumentaria
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#action/3.4">
+                Articulos deportivos
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <li class="nav-item " id="dropDown-managment">
+              <a class="nav-link" href="#lp-framework">
+                Ofertas
+              </a>
+            </li>
+
+            <li class="nav-item " id="dropDown-managment">
+              <a class="nav-link" href="#lp-test-manager">
+                Moda
+              </a>
+            </li>
+
+            <li class="nav-item " id="dropDown-managment">
+              <a class="nav-link" href="#lp-report-server">
+                Vender
+              </a>
+            </li>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
