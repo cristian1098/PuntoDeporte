75da1db6d0a629ba9c35dbf4870047b8a48b6b0b
cristian1098 <cfernandezpando@gmail.com>
2022-05-03 20:42:24 -0300

    contador con boton

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ItemCount from './ItemCount';
 import './ItemListContainer.css'
 
 const ItemListContainer = (props) => {
     const {titulo}=props
   return (
-    <div className="ItemListContainer"><h1 className="ItemListContainer-title">{titulo}</h1></div>
+    <div className="ItemListContainer"><h1 className="ItemListContainer-title">{titulo}</h1>
+    <ItemCount initial={0} stock={5} onAdd />
+    </div>
   )
 }
 
